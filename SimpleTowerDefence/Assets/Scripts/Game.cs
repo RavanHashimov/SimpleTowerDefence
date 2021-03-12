b21d9cfc318c2333211fc8f92d07acf665c09c3a
Ravan Hashimov <hashimovravan@gmail.com>
2021-03-12 18:35:01 +0400

    Basic pathfinding  with breadth-first search
    - Created GameBoard object with a custom GameBoard component that can be initialized with a 2D size.
    
    - Created Game object with a custom Game component that is in charge of the entire game.
    
    - Created GameTile prefab with a custom GameTile component which has its own quad with the arrow material.
    
    - Implemented basic pathfinding  with breadth-first search.

---
+++ SimpleTowerDefence/Assets/Scripts/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Vector2Int boardSize;
+    
+    [SerializeField] private GameBoard board;
+
+    private void Start()
+    {
+        board.Initialize(boardSize);
+    }
+}
